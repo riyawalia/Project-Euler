54369abe25d73c5ac8095853a4246f719c92e031
Riya Walia <riyawalia12@gmail.com>
2016-12-30 16:21:26 +0530

    Solved Problem 6

--- ProjectEuler.rkt
+++ ProjectEuler.rkt
@@ -22,7 +22,7 @@
 (check-expect (prime-theory 91)false)
 (check-expect (prime-theory 79)true)
 (check-expect (prime-theory 15485867) true)
-;; (my-factorial n) produces the my-factorial of consumed natural number n
+;; (my-factorial n) produces the factorial of consumed natural number n
 ;; my-factorial: Nat-> Nat
 (define (my-factorial n)
   (foldr (lambda (first rest) (* first rest)) 1 (build-list n add1)))
@@ -31,12 +31,13 @@
 (check-expect (my-factorial 0) 1)
 
 ;; (my-prime? n) checks if n is a prime.
-;; Using Wilson's Theorem
-;; my-prime?: Nat-> Nat
-;; requires: n>0
+;; Using Wilson's Theorem: if n>3 and n is a prime then (n-1)! is congruent to -1 modulus n
+;; Remember that the only 2 primes that Wilson's theorem does not hold true for is 2 and 3
+;; my-prime?: Int-> Bool
 (define (my-prime? n)
-  (and (not (= n 1)) (or (= n 2) (= n 3) 
-                         (= (remainder (my-factorial (- n 1)) n) (- n 1)))))
+  (and (not (= n 0)) (not (= (abs n) 1)) (or (= (abs n) 2) (= (abs n) 3) 
+                                             (= (remainder (my-factorial (- (abs n) 1)) (abs n)) (- (abs n) 1)))))
+(check-expect (my-prime? 0) false)
 (check-expect (my-prime? 1) false)
 (check-expect (my-prime? 2) true)
 (check-expect (my-prime? 3)true)
@@ -69,14 +70,14 @@
 ;; Find the sum of the even-valued terms in the Fibonacci sequence that do
 ;;  not exceed 4 million
 (check-expect (= (local [(define phi (/ (+ 1 (sqrt 5)) 2))
-        (define phi- (/ (- 1 (sqrt 5) )2))
-        (define (find-nth n) (/ (- (expt phi n)(expt phi- n)) (sqrt 5)))]
-  (floor  (foldr + 0 (map find-nth (build-list 12 (lambda (x) (* 3 x))))))) 4613732) true)
+                         (define phi- (/ (- 1 (sqrt 5) )2))
+                         (define (find-nth n) (/ (- (expt phi n)(expt phi- n)) (sqrt 5)))]
+                   (floor  (foldr + 0 (map find-nth (build-list 12 (lambda (x) (* 3 x))))))) 4613732) true)
 ;; Answer: 4613732
 ;;************************************
 ;; PROBLEM 5
 ;; (euler5 x) finds the smallest number that is evenly divisible by all numbers from 1..n
-;; Nat-> Nat
+;; euler5: Nat-> Nat
 ;; requires n>0
 (define (euler5 x)
   (local [(define (highest-power listoffactors x)
@@ -98,14 +99,28 @@
 (check-expect (euler5 10) 2520)  
 ;; ANSWER: 232792560
 ;;************************************
+;; PROBLEM 6
+;; (euler6 n) finds the difference between the sum of the squares of the first n number of natural
+;;   numbers and the square of the sum of the first n number of natural numbers
+;; euler6: Nat-> Nat
+
+(define (euler6 n)
+  (-
+   (expt (/ (* n (+ 1 n)) 2) 2) ;formula for sum of first n
+   (/ (* n (+ n 1) (+ 1 (* 2 n))) 6))) ;formula for sum of first n squares
+
+(check-expect (euler6 10) 2640)
+(check-expect (euler6 100) 25164150)
+;; ANSWER: 25164150
+;;************************************
 ;; PROBLEM 10
 ;; (euler10 n) finds the sum of all prime numbers less than consumed number n
 ;; euler10: Nat-> Nat
 ;; requires n>3
 (define (euler10 n)
-(local [(define limit (ceiling (/ (- n 1) 6)))]
- (+ 5 (foldr + 0 (append (filter prime? (build-list limit (lambda (x) (+ 1 (* 6 x)))))
-          (filter prime? (build-list limit (lambda (x) (- (* 6 x) 1)))))))))
+  (local [(define limit (ceiling (/ (- n 1) 6)))]
+    (+ 5 (foldr + 0 (append (filter prime? (build-list limit (lambda (x) (+ 1 (* 6 x)))))
+                            (filter prime? (build-list limit (lambda (x) (- (* 6 x) 1)))))))))
 (check-expect (euler10 5) 5)
 (check-expect (euler10 10) 17)
 (check-expect (euler10 2000000) 142913828922)
@@ -118,5 +133,9 @@
 (define (euler48 n y)
   (remainder (- (foldr + 0 (build-list (add1 n) (lambda (n) (expt n n)))) 1) (expt 10 y)))
 
+(check-expect (euler48 0 0)0)
+(check-expect (euler48 0 9)0)
+(check-expect (euler48 10 0)0)
 (check-expect (euler48 10 11) 10405071317)
 (check-expect (euler48 1000 10) 9110846700)
+;; ANSWER: 9110846700
